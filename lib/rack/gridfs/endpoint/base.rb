d507e43f0f7ad81281fa6d92e147cb4dc6214c50
Kevin <kevin.melchert@gmail.com>
2012-11-12 08:09:24 +0100

    better id check. also allows non BSON::ObjectIds

--- lib/rack/gridfs/endpoint/base.rb
+++ lib/rack/gridfs/endpoint/base.rb
@@ -64,7 +64,8 @@ module Rack
         def find_file(id_or_path)
           case @lookup.to_sym
           when :id
-            Mongo::Grid.new(db, @fs_name).get(BSON::ObjectId.from_string(id_or_path))
+            id = BSON::ObjectId.legal?(id_or_path) ? BSON::ObjectId.from_string(id_or_path) : id_or_path
+            Mongo::Grid.new(db, @fs_name).get(id)
           when :path
             path = CGI::unescape(id_or_path)
             Mongo::GridFileSystem.new(db, @fs_name).open(path, "r")
